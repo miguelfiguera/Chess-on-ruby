4ffac3a65483c03ad2495cb28770d2dbc7749514
Miguel Figuera <miguelqui725@gmail.com>
2022-09-21 18:35:21 -0400

    Working on the piece movements methods

--- Squares.rb
+++ Squares.rb
@@ -2,7 +2,7 @@ class Squares
     attr_accessor :piece
     attr_reader :position
 
-    def initialize(position,piece)
+    def initialize(position,piece=nil)
         @position = position
         @piece = piece
     end
--- game.rb
+++ game.rb
@@ -1,17 +1,31 @@
 require_relative '../pieces.rb'
 require_relative '../display.rb'
+require 'pry-byebug'
 
 class Game
-    @@white_instances=[]
-    @@black_instances=[]
-    @@squares_instances=[]
-    @@death_ones=[]
 
+    attr_accessor :display, :white_instances, :black_instances, :death_ones, :player1, :player2, :current_player
 
     def initialize
         @display=Display.new
+        @white_instances=[]
+        @black_instances=[]
+        @squares_instances=[]
+        @death_ones=[]
+        @player1=create_a_player('white')
+        @player2=create_a_player('black')
+        @current_player=nil
     end
 
+    #players
+    def create_a_player(color)
+    name = gets.chomp 
+    Player.new(name,color)
+    end
+
+    def swap_player
+        @current_player == @player1 ? @current_player=@player2 : @current_player=@player1
+    end
 
     #The pieces...
 
@@ -21,29 +35,49 @@ class Game
     end
 
     def create_black_pieces
-
+        color='black'
+        creating_all_pawns(color)
+        knight('K2',color,[7,8])
+        knight('K1',color,[2,8])
+        tower('T2',color,[8,8])
+        tower('T1',color,[1,8])
+        bishop('B1',color,[3,8])
+        bishop('B2',color,[6,8])
+        queen('Q',color,[4,8])
+        king('K',color,[5,8])
     end
 
     def create_white_pieces
+        color = 'white'
+        creating_all_pawns('white')
+        knight('K2',color,[7,1])
+        knight('K1',color,[2,1])
+        tower('T2',color,[8,1])
+        tower('T1',color,[1,1])
+        bishop('B1',color,[3,1])
+        bishop('B2',color,[6,1])
+        queen('Q',color,[4,1])
+        king('K',color,[5,1])
     end
 
     #Pieces one by one
 
-    def the_pushing(color,piece)
-       @@white_instances.push(piece) if color == 'white'
-       @@black_instances.push(piece) if color == 'black'
+    def the_pushing(piece,color)
+       @white_instances.push(piece) if color == 'white'
+       @black_instances.push(piece) if color == 'black'
     end
 
     def pawn(name,color,position)
-        Pawn.new(name,color,position)
+        the_pawn=Pawn.new(name,color,position)
+        the_pushing(the_pawn,color)
     end
 
     def creating_all_pawns(color)
         starting=nil
         ending = nil
         if color = 'black'
-            starting=[1,6]
-            ending=[8,6]
+            starting=[1,7]
+            ending=[8,7]
         elsif color = 'white'
             starting=[1,2]
             ending = [8,2]
@@ -53,42 +87,52 @@ class Game
         loop do |pawn|
             name = letter + starting[0].to_s
             position = starting 
-            Pawn.new(name,color,position)
+            pawn(name,color,position)
             break if starting == ending
             starting[0] +=1
         end
     end
 
-
-
-    end
-
     def tower(name,color,position,moves=[0,1],[1,0])
-        Tower.new(name,color,position,moves)
+        the_tower=Tower.new(name,color,position,moves)
+        the_pushing(the_tower,color)
     end
 
     def knight(name,color,position,moves=[[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]])
-    Knight.new(name,color,position,moves)
+    the_knight=Knight.new(name,color,position,moves)
+    the_pushing(the_knight,color)
     end
 
     def bishop(name,color,position,moves=[[1,1],[-1,-1],[-1,1],[1,-1]])
-        Bishop.new(name,color,position,moves)
+        the_b=Bishop.new(name,color,position,moves)
+        the_pushing(the_b,color)
     end
 
     def king(name,color,position,moves=[[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,-1],[-1,1]])
-        King.new(name,color,position,moves)
+        the_k=King.new(name,color,position,moves)
+        the_pushing(the_k,color)
     end
 
     def queen(name,color,position,moves=[[0,y],[0,y],[x,0],[x,0],[x,y],[x,y],[x,y],[x,y]])
-        Queen.new(name,color,position,moves)
+        the_quinn=Queen.new(name,color,position,moves)
+        the_pushing(the_quinn,color)
     end
 
     # MOVES
 
     #valid? piece by piece.
 
-    def occupied?
+    def valid?(position)
+        position[0].between?(1,8) && position[1].between?(1,8)
     end
+
+    def occupied?(position)
+        @squares_instances.each do |sp|
+            return true if sp.position == position
+        end
+        false 
+    end
+
 
     def pawn_valid?
     end
@@ -123,8 +167,8 @@ class Game
 
     #Finding a Piece
     def finding_piece(name,color)
-        arr1=@@white_instances
-        arr2=@@black_instances
+        arr1=@white_instances
+        arr2=@black_instances
         result=nil
 
         if color == 'white'
@@ -149,8 +193,8 @@ class Game
     end
 
     def finding_piece_for_killing(position)
-        arr1= @@black_instances 
-        arr2= @@white_instances
+        arr1= @black_instances 
+        arr2= @white_instances
         target_index=nil
         arr1.each_with_index do |piece,index|
             target_index=index if piece.position == position
@@ -163,6 +207,35 @@ class Game
         end.
 
         return target_index if !target_index.nil?
+    end
+
+    # SQUARES METHODS
+
+    def array_of_positions(board = [], x = 1, y = 1)
+            loop do
+              arr = [x, y]
+              board.push(arr)
+              y += 1 if x == 8
+              x == 8 ? x = 1 : x += 1
+              break if board[-1] == [8, 8]
+              break if board.length > 63
+            end
+            board
+          end
+    end
+
+    def create_squares
+        array = array_of_positions
+        array.each {|position| @squares_instances.push(Squares.new(position))}
+    end
+
+    def actualize_piece
+        all_piece_instances=@black_instances + @white_instances
+        @squares_instances.each do |square|
+            all_piece_instances.each do |piece|
+                piece.position == square.position ? square.piece = piece : square.piece = nil
+            end
+        end
     end
 
 end
