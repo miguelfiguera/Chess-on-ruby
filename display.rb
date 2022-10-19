c4c8c026e5626898fef4d35dacc298d67b9aec33
Miguel Figuera <miguelqui725@gmail.com>
2022-10-19 00:16:52 -0400

    starting to work on display

---
+++ display.rb
@@ -0,0 +1,13 @@
+class Display 
+    def initialize
+        @one= Array.new(9){'   '}
+        @two= Array.new(9){'   '}
+        @three= Array.new(9){'   '}
+        @four= Array.new(9){'   '}
+        @five= Array.new(9){'   '}
+        @six= Array.new(9){'   '}
+        @seven= Array.new(9){'   '}
+        @eigth= Array.new(9){'   '}
+    end
+
+    #There are 9 elements so 
